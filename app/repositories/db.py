bea0dc6f23a8a903cc1696cd549d04ccb6818dd5
Guru <gurugastgar@gmail.com>
2026-05-12 12:16:22 +0530

    aded /coneced database

--- app/repositories/db.py
+++ app/repositories/db.py
@@ -1,4 +1,17 @@
-from sqlalchemy.orm import declarative_base
-
+from app.configs.app_config import getAppConfig
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
+
+config = getAppConfig()
+engine = create_engine(config.database_url)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- app/routes/todo.py
+++ app/routes/todo.py
@@ -1,32 +1,31 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.orm import Session
 
-
-
-from fastapi import APIRouter, HTTPException,status
-
-from app.consts.todos import allTodos as todos
+from app.repositories.db import get_db
+from app.repositories.schemas.todo_db_schema import TodoSchema
 from app.schemas.schema import Todo
 
-
-router = APIRouter(
-    prefix="/todo"
-)
+router = APIRouter(prefix="/todo")
 
 @router.get("/")
-def getTodos():
+def getTodos(db: Session = Depends(get_db)):
+    todos = db.query(TodoSchema).all()
     return todos
 
-
-
-@router.post("/addtodo",status_code=status.HTTP_201_CREATED)
-def addTodo(todo:Todo):
+@router.post("/addtodo", status_code=status.HTTP_201_CREATED)
+def addTodo(todo: Todo, db: Session = Depends(get_db)):
     try:
-        todos.append(todo)
+        new_todo = TodoSchema(content=todo.content)
+        db.add(new_todo)
+        db.commit()
+        db.refresh(new_todo)
         return {
-            "success":True,
-            "data":todo
+            "success": True,
+            "data": new_todo
         }
-    except Exception:
+    except Exception as e:
+        db.rollback()
         raise HTTPException(
-            status_code=401,
-            detail="faild to create Todo"
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Failed to create Todo"
         )
